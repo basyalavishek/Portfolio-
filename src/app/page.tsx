576368e949bbd32f462b9922ba42c7fa094b5328
Anonymous-2003 <avishekbasyal3@gmail.com>
2025-09-18 18:26:33 +0545

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,3 @@
-// import About from "../components/About";
-// import Contact from "../components/Contact";
-// import Hero from "../components/Hero";
-// import Projects from "../components/Projects";
-
-// export default function Home() {
-//   return (
-//     <>
-//       <Hero />
-//       <About />
-
-//       <Projects />
-//       <Contact />
-//     </>
-//   );
-// }
-
-// app/page.tsx
 import Hero from "../components/Hero";
 import About from "../components/About";
 import Projects from "../components/Projects";
@@ -40,6 +22,8 @@ export const metadata = {
     "Next.js",
     "Frontend",
     "Backend",
+    "avishek basyal",
+    "Avishek Basyal",
   ],
   author: "Avishek Basyal",
   openGraph: {
